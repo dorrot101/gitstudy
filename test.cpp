9ce5947a2e8c28253e5c45efc033946450210647
kimhyunwoo <kimhyunwoo101@gmail.com>
2021-02-11 00:18:34 +0900

    first commit

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 int main(){
     std::cout<<"Hello world"<<'\n';
-    std::cout<<"Hello from the other side"<<'\n';
+    std::cout<<"Hello from the out side"<<'\n';
     std::cout<<"empty commit"<<'\n';
     return 0;
 }
